d73042b099e76015c2393e13b0ffcb57472cb7e4
Jun Siang Cheah <me@deathsnacks.com>
2017-09-15 19:23:45 +0800

    Split bbs.json file to avoid 100MB file limit

--- parsers/skillparser.py
+++ parsers/skillparser.py
@@ -14,7 +14,7 @@ class SkillParser(BaseParser):
 
     def __init__(self, **data):
         super(self.__class__, self).__init__(**data)
-        self.file_name = "bbs.json"
+        self.file_name = "bbs_"
 
     def run(self):
         # Check if we have all the data we need
@@ -27,3 +27,19 @@ class SkillParser(BaseParser):
             skill_data = parse_skill(
                 self.data['skill'][skill_id], self.data['skill level'][skill_id], self.data['dict'], is_foreign, id=skill_id)
             self.parsed_data[skill_id] = skill_data
+
+    def save(self, directory):
+        """Saves parsed data into directory
+
+        Args:
+            directory: Directory to save JSON data in
+        """
+        print "Saving {} elements".format(len(self.parsed_data))
+        for starting_index in range(0, 10):
+            json_str = json.dumps(dict((k, v) for k, v in self.parsed_data.items() if k.startswith(str(starting_index))),
+                                sort_keys=True,
+                                indent=4,
+                                cls=IndentlessEncoder,
+                                ensure_ascii=False).encode('utf8').replace('\"\\\"[', '[').replace(']\\\"\"', ']')
+            with open(os.path.join(os.path.normcase(directory), self.file_name + str(starting_index)) + '.json', 'w+') as f:
+                f.write(json_str)
